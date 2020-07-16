cd4731ea3cf9d773dab2e7734380c68ed9aab3b5
99Kies <1290017556@qq.com>
2020-07-16 18:41:29 +0800

    auto update

---
+++ autoupdate_bak.py
@@ -0,0 +1,26 @@
+#-*- coding:utf-8 -*-
+"""
+    将脚本放置项目根目录下
+"""
+
+from git import Repo
+import os
+import time
+
+dirfile =  os.path.abspath('')
+repo = Repo(dirfile)
+
+for i in range(1000):
+    time.sleep(1)
+    g = repo.git
+    try:
+        g.add("--all")
+        g.commit('-m auto update')
+        g.push()
+    except:
+        try:
+            g.pull()
+            g.push()
+        except:
+            print('Already Update')
+    print('Successful push!')
